d523c6e6bb542cb6bdfdee6a0db3e4662c575622
jkkCoder <jayeshk118@gmail.com>
2025-12-22 13:26:16 +0530

    fix

--- src/application/shared/ports/password-hasher.port.ts
+++ src/application/shared/ports/password-hasher.port.ts
@@ -1,6 +1,6 @@
 export interface PasswordHasherPort {
-  hash(password: string): Promise<string>;
-  verify(password: string, hash: string): Promise<boolean>;
+  hash(password: string, userName?: string): Promise<string>;
+  verify(password: string, hash: string, userName?: string): Promise<boolean>;
 }
 
 // Token for dependency injection
